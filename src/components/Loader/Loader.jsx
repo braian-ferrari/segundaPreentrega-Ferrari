eab868cd6fa3480d01e323317f928e1856ce0f09
Brachu <braian.ferrari.bf@gmail.com>
2024-05-26 00:39:02 -0300

    adicione css

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
 import 'animate.css';
+import styles from './Loader.module.css';
+
 const Loader = () => {
   return (
-    <div className='animate__animated animate__bounce'>🏀</div>
-  )
-}
+    <div className={`animate__animated animate__bounce animate__infinite ${styles.Loader}`}>🏀</div>
+  );
+};
 
-export default Loader
+export default Loader;
