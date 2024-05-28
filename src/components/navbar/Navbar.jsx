689771af1a7f76cfa32c31c9c9ed0ea98888657a
Brachu <braian.ferrari.bf@gmail.com>
2024-05-28 12:40:42 -0300

    modifique el responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
             <div className="container-fluid">
                 <a className="navbar-brand" href="#"></a>
                 <button className={`navbar-toggler ${styles.btn}`} type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className={`navbar-toggler ${styles.icono}`}>🏀</span>
+                    <span className={`navbar-toggler-icon ${styles.icono}`}></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
